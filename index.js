39247e8954851ba9135943a35f08c81a29dfe6d1
sabrina-h3 <sabrinah3llen@gmail.com>
2023-12-17 03:05:19 -0300

    desafio realizado

---
+++ index.js
@@ -0,0 +1,16 @@
+let nomeHeroi = ("Digite o nome do heroi: ");
+let xpDoHeroi = ("Digite a quantidade de XP do heroi: ");
+
+let nivel;
+const limites = [1000, 2000, 5000, 7000, 8000, 9000, 10000];
+const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
+
+for (let i = 0; i < limites.length; i++) {
+    if (xpHeroi <= limites[i]) {
+        nivel = niveis[i];
+        break;
+    }
+}
+
+nivel = nivel || "Radiante";
+console.log("O heroi de nome **{nomeHeroi}** está no nível de **{xpDoHeroi}**")
